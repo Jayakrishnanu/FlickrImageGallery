3153e441b1d1e40495c80b507f1373e468f8e1ca
Jay <jayakrishnansb@gmail.com>
2024-09-18 09:29:57 -0700

    Initial Commit

---
+++ FlickrImageGallery/FlickrImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrImageGalleryApp.swift
+//  FlickrImageGallery
+//
+//  Created by Jayakrishnan u on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
